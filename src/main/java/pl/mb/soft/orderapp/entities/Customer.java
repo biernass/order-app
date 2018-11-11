3f657979259afe62608c6ef602b28273327226aa
Michał Biernacki <micbiernacki@gmail.com>
2018-11-11 19:50:22 +0100

    Customer entity

---
+++ src/main/java/pl/mb/soft/orderapp/entities/Customer.java
@@ -0,0 +1,102 @@
+package pl.mb.soft.orderapp.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private CustomerType customerType;
+
+    private String login;
+
+    private String password;
+
+    private Boolean active;
+
+    private String token;
+
+    public Customer(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/pl/mb/soft/orderapp/entities/CustomerType.java
@@ -0,0 +1,6 @@
+package pl.mb.soft.orderapp.entities;
+
+public enum CustomerType {
+    REGULAR,
+    VIP
+}
